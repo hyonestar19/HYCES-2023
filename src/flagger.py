de202a3f1d4840eb793da98079b272d2d44f25d0
hyonestar19 <hanbyeol8378@hanyang.ac.kr>
2023-09-28 16:16:40 +0900

    flagger추가 및 purify 코드 가완성

---
+++ src/flagger.py
@@ -0,0 +1,23 @@
+### THIS FILE GIVES YOU A FLAG
+# import
+import time
+
+def main(MINUTES, pNAME):
+    ## VARIABLE CHECK
+    if(type(MINUTES) != int):
+        print("MINUTE must set as an integer!!")
+        msg = "ERROR: MINUTE doens't correspond with integer type"
+        return msg
+    
+    if(type(pNAME) != str):
+        print("pNAME must set as a string")
+        msg = "ERROR: pNAME doesn't correspond with string type"
+        return msg
+    
+    ## Calc Minutes
+    while(MINUTES != 0):
+        MINUTES -= 1
+        time.sleep(1)
+    
+    flag = pNAME + "'s timer is done"
+    return flag
--- src/purify.py
+++ src/purify.py
@@ -1,14 +1,25 @@
 # import
 import gpiozero as gp
-import time
-import random as rd
-
 
 # Variable
 TURBIN = gp.DigitalOutputDevice(17)
 
-    
+# MAIN
+## Need to give him bool parameter.
+def main(ISTURBINWORK):
+    ## CHECKING PARAMETER IS CORRECT
+    if(type(ISTURBINWORK) != bool):
+        print("You give me wrong DATA TYPE!!")
+        print("Please check your code!!")
+        msg = "ERROR: Wrong Data Type!!"
+        
+    else:
+        if(ISTURBINWORK): # 터빈이 움직여야 할 때
+            msg = "Turbin is working for 20 minutes."
+            TURBIN.on()
+            
+        else: # 터빈이 멈출 때
+            msg = "Turbin turns off."
+            TURBIN.off()
 
-def main():
-    TURBIN.on()
-    time.sleep(1)
+    return msg
