98300095f9279a734a275706ccaedbccbd80d07c
hyonestar19 <hanbyeol8378@hanyang.ac.kr>
2023-09-28 15:55:16 +0900

    양식 수정

--- src/purify.py
+++ src/purify.py
@@ -1,6 +1,25 @@
 # import
 import gpiozero as gp
+import time
+
+# Temporary Variable
+TBW = 1 # Notify when the turbin working -> if 1 : Time to work / if 0 : Not yet
+
+
+# define
+def ISFURIFIERWORKING():
+    return TBW
 
 
 # Pin-Setup
 TURBIN = gp.DigitalOutputDevice(17)
+
+
+# Main part of the code
+while True:
+    if ISFURIFIERWORKING() == 1:
+        TURBIN.on()
+    else:
+        TURBIN.off()
+    time.sleep(1)
+    print("Iterated\n")
